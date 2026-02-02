649d9e35324bfe7e654616d398e791b25c83a77b
liuZhou <2047379328@qq.com>
2026-02-02 23:01:54 +0800

    fix(pool): Update New function to return error for invalid capacity and initialize worker cache pool

--- tests/pool_test.go
+++ tests/pool_test.go
@@ -1,13 +1,14 @@
 package tests
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/spinvettle/gcp"
 )
 
 func BenchmarkPool(b *testing.B) {
-	pool := gcp.New(100)
+	pool, _ := gcp.New(100)
 	defer pool.ShutDown()
 	// var wg sync.WaitGroup
 	for i := 0; i < b.N; i++ {
@@ -15,6 +16,31 @@ func BenchmarkPool(b *testing.B) {
 		for {
 			err := pool.Submit(func() {
 				_ = 1 + 1
+			})
+			if err == nil {
+				break
+				// b.Fatal(err.Error())
+
+				// wg.Done()
+			}
+		}
+
+	}
+	pool.ShutDown()
+	// wg.Wait()
+}
+func TestPool(b *testing.T) {
+	pool, _ := gcp.New(2)
+	defer pool.ShutDown()
+	// var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		// wg.Add(1)
+		for {
+			err := pool.Submit(func() {
+				for i := 0; i < 10000; i++ {
+					_ = i * i
+				}
+				fmt.Println(i)
 			})
 			if err == nil {
 				break
